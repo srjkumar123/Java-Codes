fce0418fd4273e9b4e8053c845f1203fdadbbad6
Suraj Kumar <121860805+srjkumar123@users.noreply.github.com>
2024-08-03 11:24:44 +0530

    Rectangle

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+
+public class Rectangle {
+
+    //member variables
+    double width;
+    double height;
+
+    //eNUM CREATED
+    enum Color {
+        RED, GREEN, BLUE
+    }
+
+    //member variable boxColor of type Color
+    Color boxColor;
+
+    //main method
+    public static void main(String[] args) {
+        //print Color.Blue
+        System.out.println(Color.BLUE);
+    }
+
+
+}
