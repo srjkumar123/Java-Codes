6b48053433a378a241e1b0985525fe68ac7dd952
Suraj Kumar <121860805+srjkumar123@users.noreply.github.com>
2024-08-03 11:21:09 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,13 @@
+package com.anudip.learning;
+
+public class Person {
+    //member variables
+    String name = "Suraj Kumar";
+    int age = 22;
+     int salary = 50000;
+
+    public static void main(String[] args) {
+        System.out.println("Test Successfull");
+    }
+
+}
