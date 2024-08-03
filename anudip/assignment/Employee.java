17e06cdc95d759c1edb80be257f18c9e4727a82a
SurajKumar <srjkumar9891@gmail.com>
2024-08-03 11:17:44 +0530

    first commit

---
+++ anudip/assignment/Employee.java
@@ -0,0 +1,15 @@
+package anudip.assignment;
+
+public class Employee {
+
+    //member variables
+    protected int id;
+    //int age = 35.5; //error
+    protected float age = 35.5F;
+    protected String name;
+    protected boolean isPermanent;
+
+    public static void main(String[] args) {
+        System.out.println("Successfully Started");
+    }
+}
